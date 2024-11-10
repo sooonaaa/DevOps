45d86cee3cafd40c66168b75b1ca5b0223333207
Sofya <sofja.cochergina@yandex.ru>
2024-11-11 01:39:33 +0300

    Добавлен модуль strings.py, удален text

--- 1.py
+++ 1.py
@@ -1,10 +1,12 @@
 def plus(a,b):
     return a+b
 
-text =("New text")
-
 def minus(a,b):
     return a-b
+
+def mul(a,b):
+    return a*b
 if __name__ == '__main__':
     print(plus(2,2))
     print(minus(4,1))
+    print(mul(3,9))
